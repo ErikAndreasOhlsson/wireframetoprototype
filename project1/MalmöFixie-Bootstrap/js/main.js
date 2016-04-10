cab92f641f93e1bf485e6d03a69fd225aafb4510
Andreas Romlin <andreas.romlin@live.se>
2016-04-10 20:34:56 +0200

    Added buildABike.html and a main.js

---
+++ project1/MalmöFixie-Bootstrap/js/main.js
@@ -0,0 +1,23 @@
+
+/**
+ * Created by andreasromlin on 2016-04-10.
+ */
+$(function () {
+
+    $('#bikeClick').on('click', function() {
+        $('#myModal').modal('show')
+
+    });
+
+    $('.color').on('click', function () {
+        var x = $(this).css('backgroundColor');
+        $('#myModal').css('backgroundColor', x);
+        $('#progressBar').removeClass('hidden');
+        setTimeout(function() {
+            $('#myModal').modal('hide').css('backgroundColor', 'white');
+            $('#progressBar').addClass('hidden');
+        }, 2000);
+
+    });
+
+});
